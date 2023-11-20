2428ed5c0bf9140d06d7c3c6f1abb00a506938cf
Rafael Louie Miguel <118577897+egg-lou@users.noreply.github.com>
2023-11-20 09:06:11 +0800

    done

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 data = [
-    {'Topic': 'Topic 1', 'Image': '![Image 1](./images/1.jpg)'},
-    {'Topic': 'Topic 2', 'Image': '![Image 2](./images/2.jpg)'},
-    {'Topic': 'Topic 3', 'Image': '![Image 3](./images/3.jpg)'},
+    {'Topic': 'Topic 1', 'Image': '![Image 1](./images/1.jpg)', 'Content': 'Content 1'},
+    {'Topic': 'Topic 2', 'Image': '![Image 2](./images/2.jpg)', 'Content': 'Content 2'},
+    {'Topic': 'Topic 3', 'Image': '![Image 3](./images/3.jpg)', 'Content': 'Content 3'},
 ]
 
-markdown_content = "| Topic | Image |\n| --- | --- |\n"
+markdown_content = "| Topic | Image | Content |\n| --- | --- | --- |\n"
 for row in data:
-    markdown_content += f"| {row['Topic']} | {row['Image']} |\n"
+    markdown_content += f"| {row['Topic']} | {row['Image']} | {row['Content']} |\n"
 
 markdown_name = 'tables.md'
 
